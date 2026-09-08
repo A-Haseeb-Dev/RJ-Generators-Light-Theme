8b954cb210512ad4a530fb457f1061a443bea1a1
A-Haseeb-Dev <muhammadhaseebkhalid029@gmail.com>
2026-09-08 19:41:05 +0500

    Update buyback email address

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -110,7 +110,7 @@ export default function ContactPage() {
                       <div>
                         <h3 className="font-bold mb-1">Email</h3>
                         <p className="text-zinc-600 text-sm">zehakmujeeb@icloud.com</p>
-                        <p className="text-zinc-600 text-sm">buyback@rjgenerators.com</p>
+                        <p className="text-zinc-600 text-sm">zehakmujeeb@icloud.com</p>
                       </div>
                     </li>
                   </ul>
