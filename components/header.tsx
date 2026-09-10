dbd775d105d7a7c536389c0c21bb62e1bdbaaf2e
Muhammad Haseeb Khalid <101590284+A-Haseeb-Dev@users.noreply.github.com>
2026-09-10 18:15:31 +0500

    Update header image height for better display

--- components/header.tsx
+++ components/header.tsx
@@ -66,7 +66,7 @@ export function Header({ theme = "light" }: HeaderProps) {
             <img
               src="/images/RJ-Logo.png"
               alt="RJ Generator"
-              className="h-20 md:h-24 w-auto object-contain transition-transform group-hover:scale-105"
+              className="h-24 md:h-24 w-auto object-contain transition-transform group-hover:scale-105"
             />
           </Link>
 
